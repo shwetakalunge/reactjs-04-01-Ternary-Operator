8e4b8b07358b9aadbf573a1a6e35d13309f51d96
shwetakalunge <shwetak@WinjitTechnologiesPvtLtd.local>
2021-01-04 17:13:03 +0530

    first commit

---
+++ src/Heading.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Heading = (props) => {
+  return <h3 className="card__title">{props.sname32} </h3>;
+};
+
+export default Heading;
---
+++ src/Sdata.jsx
@@ -0,0 +1,34 @@
+const Sdata = [
+  {
+    sname: "DRAK",
+    imgscr: "https://picsum.photos/id/1/200/300",
+    title: "NETFLIX ORIGINAL SERIES",
+    links: "https://theworldpursuit.com/sunset-captions/",
+  },
+  {
+    sname: "GOT",
+    imgscr: "https://picsum.photos/id/1/200/300",
+    title: "NETFLIX ORIGINAL SERIES",
+    links: "https://theworldpursuit.com/sunset-captions/",
+  },
+  {
+    sname: "Avengers",
+    imgscr: "https://picsum.photos/id/1/200/300",
+    title: "NETFLIX ORIGINAL SERIES",
+    links: "https://theworldpursuit.com/sunset-captions/",
+  },
+  {
+    sname: "ATTTT",
+    imgscr: "https://picsum.photos/id/1/200/300",
+    title: "NETFLIX ORIGINAL SERIES",
+    links: "https://theworldpursuit.com/sunset-captions/",
+  },
+  {
+    sname: "TENET",
+    imgscr: "https://picsum.photos/id/1/200/300",
+    title: "NETFLIX ORIGINAL SERIES",
+    links: "https://theworldpursuit.com/sunset-captions/",
+  },
+];
+
+export default Sdata;
